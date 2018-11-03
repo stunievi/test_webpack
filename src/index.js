a86a30bf1fac5b6be952852494264cc4f03fafcd
Matt <ievi@live.com>
2018-11-03 16:01:00 +0800

    add HMR

--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
  import _ from 'lodash';
  import printMe from './print.js';
-
-  function component() {
+import './style.css';
+function component() {
     var element = document.createElement('div');
-   var btn = document.createElement('button');
+    var btn = document.createElement('button');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-   btn.innerHTML = 'Click me and check the console!';
-   btn.onclick = printMe;
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;  // onclick event is bind to the original printMe function
 
-   element.appendChild(btn);
+    element.appendChild(btn);
 
     return element;
-  }
+}
 
-  document.body.appendChild(component());
+let element = component();
+document.body.appendChild(element);
+
+if (module.hot) {
+    module.hot.accept('./print.js', function () {
+        console.log('Accepting the updated printMe module!');
+        document.body.removeChild(element);
+        element = component(); // Re-render the "component" to update the click handler
+        document.body.appendChild(element);
+    })
+}
